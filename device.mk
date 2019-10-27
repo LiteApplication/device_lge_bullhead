9a62bdb2ced016ae9945330c7ba8aebc7f362fca
M. Ángel <gnuequal@gmail.com>
2019-10-27 13:33:05 +0100

    bullhead: uprev keymaster to 4.0
    Signed-off-by: M. Ángel <gnuequal@gmail.com>

--- device.mk
+++ device.mk
@@ -298,10 +298,9 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
-# Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8992
     
