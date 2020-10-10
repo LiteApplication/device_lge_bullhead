ff0a65d598bbc7517fa2c1697f423394d3699f80
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-10-10 17:16:19 +0200

    device: build vibrator service

--- device.mk
+++ device.mk
@@ -380,7 +380,8 @@ endif
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 #USB HAL
 PRODUCT_PACKAGES += \
