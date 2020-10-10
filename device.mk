eb14c002201d3156e611b9648e4084e94637f9f6
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-10-10 16:54:19 +0200

    health: use newer HAL

--- device.mk
+++ device.mk
@@ -298,8 +298,8 @@ PRODUCT_PACKAGES += \
     
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-service \
-    android.hardware.health@1.0-impl
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
