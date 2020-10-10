4bb74df5b1d9e2b633621402a0763a78ea952236
Alexis Rossfelder <contact.liteapplication@gmail.com>
2020-10-10 17:05:40 +0200

    bullhead: add bluetooth HAL and service

--- device.mk
+++ device.mk
@@ -588,7 +588,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
